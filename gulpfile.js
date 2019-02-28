90622cd90d34ed5ba8fb3ad7279ebf812e432259
Sky Rousse <skyrousse@gmail.com>
2019-02-28 14:31:44 -0800

    add updated sass packages and module configuration
    updating sass utils to be more up to date and to work with Gulp4 configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,100 @@
-//gulpfile.js
+/**************** Gulp.js 4 configuration ****************/
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    livereload = require('gulp-livereload');
+const
 
-//style paths
-var sassFiles = 'assets/styles/sass/**/*.scss',
-    cssDest = 'assets/styles/css/';
+  // development or production
+  devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
 
-livereload({ start: true })
+  // directory locations
+  dir = {
+    src: 'src/',
+    build: 'build/'
+  },
 
-gulp.task('styles', function() {
-  gulp.src(sassFiles)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(cssDest))
-    .pipe(livereload());
-});
+  // modules
+  gulp = require('gulp'),
+  // sass modules
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  // util modules
+  browsersync = devBuild ? require('browser-sync').create() : null,
 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch(sassFiles,['styles']);
-});
+
+/**************** CSS task ****************/
+
+const cssConfig = {
+
+  src: dir.src + 'scss/main.scss',
+  watch: dir.src + 'scss/**/*',
+  build: dir.build + 'css/',
+  sassOpts: {
+    sourceMap: devBuild,
+    outputStyle: 'nested',
+    imagePath: '/images/',
+    precision: 3,
+    errLogToConsole: true
+  },
+
+  postCSS: [
+    require('postcss-assets')({
+      loadPaths: ['images/'],
+      basePath: dir.build
+    }),
+    require('autoprefixer')({
+      browsers: ['> 1%']
+    })
+  ]
+
+};
+
+// remove unused selectors and minify production CSS
+if (!devBuild) {
+
+  cssConfig.postCSS.push(
+    require('usedcss')({ html: ['index.html'] }),
+    require('cssnano')
+  );
+
+}
+
+function css() {
+
+  return gulp.src(cssConfig.src)
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+    .pipe(postcss(cssConfig.postCSS))
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(cssConfig.build))
+    .pipe(browsersync ? browsersync.reload({ stream: true }) : noop());
+
+}
+exports.css = css;
+
+
+/**************** server task (now private) ****************/
+
+const syncConfig = {
+  server: {
+    baseDir: './build',
+    index: 'index.html'
+  },
+  port: 8000,
+  open: false
+};
+
+
+/**************** watch task ****************/
+
+function watch(done) {
+
+  // CSS changes
+  gulp.watch(cssConfig.watch, css);
+
+  done();
+
+}
+
+/**************** default task ****************/
+
+exports.default = gulp.series(exports.css, watch, server);
