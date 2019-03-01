40e462781f9e041d870e8559124771019b158142
Sky Rousse <skyrousse@gmail.com>
2019-02-28 21:06:46 -0800

    add js module for toggling the mobile nav and update position of close icon

--- src/js/comonents/navToggle.js
+++ src/js/comonents/navToggle.js
@@ -1,7 +1,23 @@
 import $ from 'jquery';
 
+const selectors = {
+  hamburgerIcon: '.mobile-nav-toggle',
+  closeNavIcon: '.pullout-close',
+  nav: '#mobile-nav'
+}
+
+const open = ($el) => {
+  $el.slideDown();
+}
+
+const close = ($el) => {
+  $el.slideUp();
+}
+
 const navToggle = () => {
-  $('findNave').toggle();
+  const $nav = $(selectors.nav);
+  $(selectors.hamburgerIcon).on('click', () => open($nav));
+  $(selectors.closeNavIcon).on('click', () => close($nav));
 }
 
 export default navToggle;
